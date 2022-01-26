b75ed43246dc0b06bdc32a0c184f8f9095e423f2
mustak <mustakpatel278@gmail.com>
2022-01-26 16:10:46 +0530

    added two var

--- sum.java
+++ sum.java
@@ -1,6 +1,8 @@
 public class sum {
     public static void main(String[] args)
     {
+        int x=40;
+        int y=30;
         System.out.println("hello");
         System.out.println(30+30);
     }
