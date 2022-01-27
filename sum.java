3a4744396f38f8816069b5e738bc5897bd33a6f4
mustak <mustakpatel278@gmail.com>
2022-01-27 17:33:21 +0530

    add new push method

--- sum.java
+++ sum.java
@@ -1,8 +1,14 @@
 public class sum {
+    public static void push()
+    {
+        System.out.println("hello i'm push method");
+    }
     public static void main(String[] args)
     {
         String s="musa";
         System.out.println(30+30);
         System.out.println("hello im neew member");
+        push();
+
     }
 }
