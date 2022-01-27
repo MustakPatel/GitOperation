b4e32ad01786ea415797ed7616c447e31ec669e3
mustak <mustakpatel278@gmail.com>
2022-01-27 15:24:53 +0530

    fixed variable problem

--- sum.java
+++ sum.java
@@ -1,9 +1,7 @@
 public class sum {
     public static void main(String[] args)
     {
-        int x=40;
-        int y=30;
-
+        String s="musa";
         System.out.println(30+30);
         System.out.println("hello im neew member");
     }
